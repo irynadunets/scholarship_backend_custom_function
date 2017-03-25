b1aa689478a29b2cac04ea57175fa1a3f546c757
irynadunets <idunets@yahoo.com>
2017-03-25 00:48:52 -0700

    Add files via upload

---
+++ scholarship/src/java/service/AbstractFacade.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import scholarship.Scholarship;
+import static scholarship.Scholarship_.country;
+import static scholarship.Scholarship_.field;
+import static scholarship.Scholarship_.language;
+import static scholarship.Scholarship_.level;
+/**
+ *
+ * @author ARI
+ */
+public abstract class AbstractFacade<T> {
+    private Class<T> entityClass;
+
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    protected abstract EntityManager getEntityManager();
+
+    public void create(T entity) {
+        getEntityManager().persist(entity);
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    public List<T> findRange(int[] range) {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+
+    public int count() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+    
+    public List<T> getScholarshipfindByField(Object level,Object field,Object country,Object language) {
+      CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+      CriteriaQuery cq = cb.createQuery();
+      Root<T> c = cq.from(entityClass);      
+      cq.select(c);   
+      cq.where(cb.equal(c.get("level"), level),cb.equal(c.get("field"), field),cb.equal(c.get("country"), country),cb.equal(c.get("language"), language));
+      return getEntityManager().createQuery(cq).getResultList();
+      
+    }
+
+    
+    
+    
+}
